c5b998f343df4f814fd974fe6978e90da37f8df8
Marck-V <42965997+Marck-V@users.noreply.github.com>
2025-04-15 22:38:26 -0700

    added func_godot plugin + some shaders

---
+++ LevelManager.gd
@@ -0,0 +1,31 @@
+extends Node
+
+@onready var door: CSGBox3D = $'../Door'
+@onready var key: Area3D = $"../Key"
+@onready var door2: CSGBox3D = $'../Door2'
+
+var has_key = false
+var door2_unlocked = false  # Flag to track if door2 has been unlocked
+
+func _process(delta):
+	if has_key:
+		unlock_door()
+	else:
+		if not door2_unlocked and get_enemy_count() == 0:  # Check if door2 is already unlocked
+			door2.queue_free()
+			door2_unlocked = true  # Set the flag to prevent further calls
+			print("All enemies defeated, door unlocked.")
+
+func _on_key_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		print(body)
+		has_key = true
+		key.queue_free()
+
+func unlock_door():
+	door.queue_free()
+	has_key = false
+	
+func get_enemy_count():
+	var enemies = get_node("../Enemies")
+	return enemies.get_child_count()
--- objects/player.gd
+++ objects/player.gd
@@ -122,10 +122,11 @@ func handle_controls(_delta):
 		mouse_captured = true
 	
 	if Input.is_action_just_pressed("mouse_capture_exit"):
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		mouse_captured = false
+		# Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		# mouse_captured = false
 		
-		input_mouse = Vector2.ZERO
+		# input_mouse = Vector2.ZERO
+		get_tree().quit()
 	
 	# Movement
 	
