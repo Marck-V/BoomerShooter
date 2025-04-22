490d8bfea9fb3fa7841c6fe8cef36d63ddd33f3f
BCThunder <thunder3000lite@gmail.com>
2025-04-22 11:25:40 -0700

    Added ranged enemy
    Ranged enemy can shoot energy blast at player when in range and deal damage.
    
    Bug: enemy will keep chasing player until its touching the player. I want to make them stop chasing when its at around maximum firing range.

--- objects/energy_ball.gd
+++ objects/energy_ball.gd
@@ -1,4 +1,4 @@
-extends RigidBody3D
+extends Area3D
 
 @onready var mesh_instance_3d = $MeshInstance3D
 @onready var collision_shape_3d = $CollisionShape3D
@@ -10,3 +10,16 @@ func _ready():
 	
 func _physics_process(delta):
 	position += transform.basis * Vector3(0, 0, SPEED) * delta
+	
+
+func _on_body_entered(body):
+	if body.has_method("damage"):
+		Audio.play("sounds/enemy_attack.ogg")
+#
+		body.damage(40)  # Apply damage to player
+	
+	queue_free()
+
+# Destroy itself if it never collides
+func _on_self_queue_timer_timeout():
+	queue_free()
