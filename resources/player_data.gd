80cc1ad47b83677b76a6dcb8082a16f11215fc80
Marck-V <42965997+Marck-V@users.noreply.github.com>
2025-10-03 13:59:58 -0700

    upgrade menu in progress

--- resources/player_data.gd
+++ resources/player_data.gd
@@ -5,7 +5,13 @@ class_name PlayerData
 @export var upgrades: Dictionary = {
 	"pistol_upgrade1" : false,
 	"pistol_upgrade2" : false,
-	"pistol_upgrade3" : false
+	"pistol_upgrade3" : false,
+	"shotgun_upgrade1" : false,
+	"shotgun_upgrade2" : false,
+	"shotgun_upgrade3" : false,
+	"rifle_upgrade1" : false,
+	"rifle_upgrade2" : false,
+	"rifle_upgrade3" : false
 }
 @export var ammo: Dictionary = {
 	"pistol": 50,
--- scripts/ui/upgrade_menu.gd
+++ scripts/ui/upgrade_menu.gd
@@ -5,10 +5,23 @@ var blaster : Weapon = load("res://resources/weapons/pistol.tres")
 var shotgun : Weapon = load("res://resources/weapons/shotgun.tres")
 var rifle : Weapon = load("res://resources/weapons/rifle.tres")
 var damage_increase = 5
-const BLASTER_PATH = "res://resources/weapons/blaster.tres"
-@onready var rifle_button_1: UpgradeButton = $RifleButton1
-@onready var rifle_button_2: UpgradeButton = $RifleButton1/RifleButton2
-@onready var rifle_button_3: UpgradeButton = $RifleButton1/RifleButton2/RifleButton3
+
+const PISTOL_PATH = "res://resources/weapons/pistol.tres"
+const SHOTGUN_PATH = "res://resources/weapons/shotgun.tres"
+const RIFLE_PATH = "res://resources/weapons/rifle.tres"
+
+# Pistol
+@onready var pistol_button_1: UpgradeButton = $Pistol/PistolButton1
+@onready var pistol_button_2: UpgradeButton = $Pistol/PistolButton1/PistolButton2
+@onready var pistol_button_3: UpgradeButton = $Pistol/PistolButton1/PistolButton2/PistolButton3
+# Shotgun
+@onready var shotgun_button_1: UpgradeButton = $Shotgun/ShotgunButton1
+@onready var shotgun_button_2: UpgradeButton = $Shotgun/ShotgunButton1/ShotgunButton2
+@onready var shotgun_button_3: UpgradeButton = $Shotgun/ShotgunButton1/ShotgunButton2/ShotgunButton3
+# Rifle
+@onready var rifle_button_1: UpgradeButton = $Rifle/RifleButton1
+@onready var rifle_button_2: UpgradeButton = $Rifle/RifleButton1/RifleButton2
+@onready var rifle_button_3: UpgradeButton = $Rifle/RifleButton1/RifleButton2/RifleButton3
 @onready var points_label: Label = $HBoxContainer/PointsLabel
 
 func _ready() -> void:
@@ -17,8 +30,23 @@ func _ready() -> void:
 	points_label.text = "Points: " + str(GlobalVariables.get_points())
 
 	GlobalVariables.points_changed.connect(on_points_changed)
+	pistol_button_1.connect("pressed", _on_pistol_button_1_pressed)
+	pistol_button_2.connect("pressed", _on_pistol_button_2_pressed)
+	pistol_button_3.connect("pressed", _on_pistol_button_3_pressed)
+	shotgun_button_1.connect("pressed", _on_shotgun_button_1_pressed)
+	shotgun_button_2.connect("pressed", _on_shotgun_button_2_pressed)
+	shotgun_button_3.connect("pressed", _on_shotgun_button_3_pressed)
+	rifle_button_1.connect("pressed", _on_rifle_button_1_pressed)
+	rifle_button_2.connect("pressed", _on_rifle_button_2_pressed)
+	rifle_button_3.connect("pressed", _on_rifle_button_3_pressed)
 
+
+	# Disable buttons if upgrade already purchased
 	if GlobalVariables.has_upgrade("pistol_upgrade1"):
+		pistol_button_1.disabled = true
+	if GlobalVariables.has_upgrade("shotgun_upgrade1"):
+		shotgun_button_1.disabled = true
+	if GlobalVariables.has_upgrade("rifle_upgrade1"):
 		rifle_button_1.disabled = true
 
 
@@ -48,16 +76,12 @@ func attempt_upgrade(button: UpgradeButton, resource: Resource, resource_path: S
 		GlobalVariables.purchase_upgrade(id)
 		button.apply_visual_upgrade()
 
-	
-func _on_rifle_button_1_pressed() -> void:	
-	attempt_upgrade(rifle_button_1, blaster, BLASTER_PATH)
-
 
 func _on_reset_button_pressed() -> void:
 	# Reset weapon stat
-	blaster.damage = 25
-	blaster.shot_count = 5
-	ResourceSaver.save(blaster, BLASTER_PATH)
+	rifle.damage = 25
+	shotgun.shot_count = 5
+	ResourceSaver.save(shotgun, SHOTGUN_PATH)
 
 	# Reset points
 	GlobalVariables.reset_points()
@@ -73,10 +97,32 @@ func _on_reset_button_pressed() -> void:
 
 	print("Upgrades, points, and button visuals reset.")
 
+# Pistol
+func _on_pistol_button_1_pressed() -> void:
+	attempt_upgrade(pistol_button_1, blaster, PISTOL_PATH)
+
+func _on_pistol_button_2_pressed() -> void:
+	attempt_upgrade(pistol_button_2, blaster, PISTOL_PATH)
+
+func _on_pistol_button_3_pressed() -> void:
+	attempt_upgrade(pistol_button_3, blaster, PISTOL_PATH)
+	
+# Shotgun
+func _on_shotgun_button_1_pressed() -> void:
+	attempt_upgrade(shotgun_button_1, shotgun, SHOTGUN_PATH)
+
+func _on_shotgun_button_2_pressed() -> void:
+	attempt_upgrade(shotgun_button_2, shotgun, SHOTGUN_PATH)
+
+func _on_shotgun_button_3_pressed() -> void:
+	attempt_upgrade(shotgun_button_3, shotgun, SHOTGUN_PATH)
+
+# Rifle
+func _on_rifle_button_1_pressed() -> void:
+	attempt_upgrade(rifle_button_1, rifle, RIFLE_PATH)
 
 func _on_rifle_button_2_pressed() -> void:
-	attempt_upgrade(rifle_button_2, blaster, BLASTER_PATH)
-
+	attempt_upgrade(rifle_button_2, rifle, RIFLE_PATH)
 
 func _on_rifle_button_3_pressed() -> void:
-	attempt_upgrade(rifle_button_3, blaster, BLASTER_PATH)
+	attempt_upgrade(rifle_button_3, rifle, RIFLE_PATH)
