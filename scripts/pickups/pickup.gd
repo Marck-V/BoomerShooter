09c474d917cb106fb8c21be446f307db717875a7
Marck-V <42965997+Marck-V@users.noreply.github.com>
2025-12-01 12:42:00 -0800

    cleaned up pickup script, increased scale for ammo for improved visibility and added some rotation and bobble.

--- scripts/pickups/pickup.gd
+++ scripts/pickups/pickup.gd
@@ -2,17 +2,21 @@ extends Node3D
 class_name BasePickup
 
 @export_enum("Health", "Ammo", "Points") var type: String
-@export_category("Ammo & Weapon")
-@export_enum("pistol","shotgun","rifle") var weapon_id: String ## Only assign if the pickup type is ammo
-@export_category("Points")
+@export_category("Weapon Type") 
+@export_enum("pistol","shotgun","rifle") var weapon_id: String ## Only assign this if this is an Ammo pickup
+@export_category("Variables")
 @export var points_amount = 500
+@export var healing_amount = 25
+@export var ammo_amount = 10
 @onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
 
 var model_scale = Vector3.ONE
 var gold_bar_scale_factor = 0.1
 var health_scale_factor = 0.5
-var healing_amount = 25
-var ammo_ammount = 300
+var ammo_scale = 2
+
+var base_y = 0.0
+var t = 0.0
 
 var ammo_paths = {
 	"pistol": "res://models/weapons/pistol_ammo_mesh.tscn",
@@ -27,23 +31,32 @@ var model_paths = {
 
 var scale_map = {
 	"Health": Vector3(health_scale_factor, health_scale_factor, health_scale_factor),
-	"Ammo": Vector3.ONE,
-	"Points": Vector3(gold_bar_scale_factor,gold_bar_scale_factor,gold_bar_scale_factor)
+	"Ammo": Vector3(ammo_scale, ammo_scale, ammo_scale),
+	"Points": Vector3(gold_bar_scale_factor, gold_bar_scale_factor, gold_bar_scale_factor)
 }
 
 func _ready() -> void:
 	mesh_instance_3d.visible = false
 	
 	var model_scene
-	var model_instance
 	if type == "Ammo":
-		model_scene = load(ammo_paths[weapon_id]) 
+		model_scene = load(ammo_paths[weapon_id])
 	else:
-		model_scene = load(model_paths[type]) 
+		model_scene = load(model_paths[type])
 	
-	model_instance = model_scene.instantiate()
+	var model_instance = model_scene.instantiate()
 	model_instance.scale = scale_map[type]
 	add_child(model_instance)
+
+	base_y = position.y
+
+func _process(delta):
+	t += delta
+
+	rotate_y(0.5 * delta)
+	position.y = base_y + sin(t) * 0.1
+	rotation.x = sin(t * 1.3) * 0.05
+	rotation.z = sin(t * 1.7) * 0.05
 	
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if not body.is_in_group("Player"):
@@ -51,9 +64,8 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 		
 	match type:
 		"Ammo":
-			GlobalVariables.add_ammo(weapon_id, ammo_ammount)
+			GlobalVariables.add_ammo(weapon_id, ammo_amount)
 			Audio.play("assets/sounds/reload.mp3")
-		
 		"Health":
 			GlobalVariables.add_health(healing_amount)
 			Audio.play("assets/sounds/health_pickup.mp3")
